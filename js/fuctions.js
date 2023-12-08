05d0105507281cb39625e4b07d5011beca17e726
checho-dev <sergiolopera.94@gmail.com>
2023-12-08 12:33:22 -0500

    Agregado el array numbers

--- js/fuctions.js
+++ js/fuctions.js
@@ -1,6 +1,13 @@
 
 const box = document.getElementById('container')
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 box.addEventListener('click',()=>{
     alert('has dado click en la caja principal')
 })
+
+number.forEach((e)=>{
+    console.log(e)
+})
+
+
